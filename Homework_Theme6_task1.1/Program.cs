16e07848f13bbbe84b327d712df8d37eaf7cf8bd
Сергей Воробьев <kazan1003@gmail.com>
2021-10-04 11:02:49 +0300

    Добавьте файлы проекта.

---
+++ Homework_Theme6_task1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Theme6_task1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
