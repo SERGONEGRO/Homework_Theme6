ee2fa7757c8d466f254d29bf0271c9631228b39d
Сергей Воробьев <kazan1003@gmail.com>
2021-10-05 13:19:33 +0300

    test

--- Homework_Theme6_task1.1/Program.cs
+++ Homework_Theme6_task1.1/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO.Compression;
+
 /// Домашнее задание
 ///
 /// Группа начинающих программистов решила поучаствовать в хакатоне с целью демонстрации
@@ -68,31 +67,37 @@ namespace Homework_Theme6_task1._1
 {
     class Program
     {
+        static string source = @"e:\Theme6_Number.txt";
+        static string groupFile = @"e:\Theme6_Groups.txt";
+        static string zipFile = @"e:\Theme6_Groups.txt.zip";
+
         static void Main(string[] args)
         {
-            int n = EnterNumber(1, 1_000_000_0);   //ввод числа от 1 до миллиарда
+            int n = EnterNumber(1, 1000000000);   //ввод числа от 1 до миллиарда
             Console.WriteLine("Кол-во чисел определено.\n");
 
             Console.WriteLine("Создание файла...\n");
-            CreateNumber(n);
+            CreateNumberFile(n,source);
+
+            //определяем кол-во групп
+            var groupCount = Math.Ceiling(Math.Log(n, 2));
 
             Console.WriteLine("\nЕсли нужно вывести только количество групп, введите '1'.");
-            
             if (Console.ReadLine().Equals("1"))   //Если ввели '1', выводим только группы:
             {
-                Console.WriteLine("\nВывод только групп\n");
+                Console.WriteLine($"\nВсего групп: {groupCount}\n");
             }
+
             else
-            {
+            {   //создаем файл с группами
+                CreateGroupsFile(n);
+                
                 Console.WriteLine("\nЕсли желаете заархивировать файл с группами, нажмите '1':\n");
                 if (Console.ReadLine().Equals("1"))
                 {
-                    Console.WriteLine("\nВывод архивированного файла с группами");
+                    CreateZipFile(groupFile ,zipFile);
                 }
-                else
-                {
-                    Console.WriteLine("\nВывод простого файла с группами");
-                }
+                
             }
                
 
@@ -125,42 +130,76 @@ namespace Homework_Theme6_task1._1
         /// Создание файла с заданным числом
         /// </summary>
         /// <param name="number">Количество чисел в файле</param>
-        static void CreateNumber(int number)
+        static void CreateNumberFile(int number, string path)
         {
-            StringBuilder sb = new StringBuilder(number); //Это быстрее всего
+            StringBuilder sb = new StringBuilder(number); 
 
             sb.Append(number);
             //запись числа в файл
-            File.WriteAllText(@"e:\HomeWork_Theme6.txt", sb.ToString());
+            File.WriteAllText(path, sb.ToString());
 
             Console.WriteLine($"Файл c числом {sb} создан.\n");
         }
+        
 
-       
+        /// <summary>
+        /// Создание файла с группами
+        /// </summary>
+        /// <param name="n">Число N</param>
+        static void CreateGroupsFile (int n)
+        {
+            DateTime dateBegin = DateTime.Now;
 
-        ///// <summary>
-        ///// Создание файла с заданным количеством чисел
-        ///// </summary>
-        ///// <param name="number">Количество чисел в файле</param>
-        //static void CreateNumber(int number)
-        //{
-        //    DateTime dateBegin = DateTime.Now;
+            StringBuilder sb = new StringBuilder(n);
+            int groupnumber = 1;
 
-        //    StringBuilder sb = new StringBuilder(number); //Это быстрее всего
+            sb.AppendFormat($"Группа №{groupnumber}:\n ");    //ввод первой группы
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == Math.Pow(2, groupnumber)) { sb.AppendFormat($"\n\nГруппа №{groupnumber + 1}:\n "); groupnumber++; }
+                sb.AppendFormat("{0} ", i);
+            }
 
-        //    for (int i = 1; i <= number; i++)
-        //    {
-        //        sb.AppendFormat("{0} ", i);
-        //    }
+            //запись массива чисел в строку
+            File.WriteAllText(groupFile, sb.ToString());
 
-        //    //запись массива чисел в строку
-        //    File.WriteAllText(@"e:\HomeWork_Theme6.txt", sb.ToString());
+            DateTime dateEnd = DateTime.Now;
+            TimeSpan secs = dateEnd - dateBegin;
 
-        //    DateTime dateEnd = DateTime.Now;
-        //    TimeSpan secs = dateEnd - dateBegin;
+            Console.WriteLine($"Файл {groupFile} создан за {secs.TotalSeconds:0.00} секунд. \n");
+        }
 
-        //    Console.WriteLine($"Файл создан. Потрачено времени: {secs.TotalSeconds} секунд. \n");
-        //}
+        /// <summary>
+        /// Архивирует файл
+        /// </summary>
+        /// <param name="input">входящий файл</param>
+        /// <param name="output">исходящий файл</param>
+        static void CreateZipFile (string input,string output)
+        {
+            DateTime dateBegin = DateTime.Now;
+
+            using (FileStream ss = new FileStream(input, FileMode.OpenOrCreate))
+            {
+                using (FileStream ts = File.Create(output))   // поток для записи сжатого файла
+                {
+                    // поток архивации
+                    using (GZipStream cs = new GZipStream(ts, CompressionMode.Compress))
+                    {
+                        ss.CopyTo(cs); // копируем байты из одного потока в другой
+                        Console.WriteLine("Сжатие файла завершено. Было: {1} байт  стало: {2} байт.",
+                                          ss.Length*2,
+                                          ts.Length*2 );
+                    }
+                }
+            }
+
+            DateTime dateEnd = DateTime.Now;
+            TimeSpan secs = dateEnd - dateBegin;
+
+            Console.WriteLine($"Файл {output} создан за {secs.TotalSeconds:0.00} секунд. \n");
+        }
+
+        
 
     }
 }
